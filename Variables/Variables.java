3e7d7ee7bdfed1f32429f0e8db1595aa9bc72f9e
Chandu <chanduthanuku24@gmail.com>
2025-07-13 13:01:04 +0530

    Create Variables.java

---
+++ Variables/Variables.java
@@ -0,0 +1,29 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        // Integer variable
+        int age = 19;
+
+        // Float variable
+        float height = 159.5f;
+
+        // Double variable
+        double weight = 44.2;
+
+        // Character variable
+        char grade = 'A';
+
+        // Boolean variable
+        boolean isStudent = true;
+
+        // String variable (not a primitive type)
+        String name = "Chandu";
+
+        // Output
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " cm");
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student? " + isStudent);
+    }
+}

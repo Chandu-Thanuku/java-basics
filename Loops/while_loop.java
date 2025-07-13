1e732f09540f4f75b5be7b0bde0c4d3088a7f511
Chandu <chanduthanuku24@gmail.com>
2025-07-13 13:18:19 +0530

    Create while_loop.java

---
+++ Loops/while_loop.java
@@ -0,0 +1,10 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        System.out.println("While loop: Print numbers 1 to 5");
+        int i = 1;
+        while (i <= 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
